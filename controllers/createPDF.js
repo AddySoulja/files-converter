4d6d6af7f4d5a2757a58cb9e27c93e38fb38352f
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-07-04 23:32:53 +0530

    files transfer controller configurations

--- controllers/createPDF.js
+++ controllers/createPDF.js
@@ -1,78 +1,75 @@
 import { PageSizes, rgb } from "pdf-lib";
 
-const createPDF = async (file, contentInJSON) => {
-  const content = JSON.parse(contentInJSON);
-  const page = file.addPage(PageSizes.A4);
-  const table = {
-    rows: Object.keys(content[0]).length,
-    columns: 2,
-    startX: 50,
-    startY: page.getHeight() - 200,
-    rowHeight: 20,
-    columnWidth: 150,
-    borderWidth: 1,
-  };
+const createPDF = async (pdfDoc, pageContent) => {
+  const content = JSON.parse(pageContent);
+  const page = pdfDoc.addPage(PageSizes.A4);
+  const fontSize = 12;
+  const padding = 8;
+  const startX = 50;
+  const startY = page.getHeight() - 50;
+  let count = 1;
+  const height = padding * 5 * count;
 
-  content.forEach((obj, objIndex) => {
-    const tableStartY = table.startY - objIndex * table.rows * table.rowHeight;
+  content.forEach((object, idx) => {
+    // Set up table parameters
+    const table = {
+      rows: 4,
+      columns: 2,
+      startX: startX + height,
+      startY: startY - idx * (fontSize + padding) * 4 + height,
+      rowHeight: fontSize + padding,
+      columnWidth: (page.getWidth() - startX * 2) / 2,
+      borderWidth: 1,
+    };
 
-    page.drawRectangle({
-      x: table.startX,
-      y: tableStartY,
-      width: table.columns * table.columnWidth,
-      height: table.rows * table.rowHeight,
-      borderWidth: table.borderWidth,
-      borderColor: rgb(0, 0, 0),
-    });
+    // Draw table borders
+    // page.drawRectangle({
+    //   x: table.startX,
+    //   y: table.startY - table.rowHeight,
+    //   width: table.columns * table.columnWidth,
+    //   height: table.rows * table.rowHeight,
+    //   borderWidth: table.borderWidth,
+    //   borderColor: rgb(0, 0, 0),
+    // });
 
-    const propertyRow = Object.keys(obj);
-    propertyRow.forEach((property, rowIndex) => {
+    // Draw table header
+    // page.drawText(`Details of Autor - ${object.name}`, {
+    //   x: table.startX + table.borderWidth,
+    //   y: table.startY + height,
+    //   size: fontSize,
+    //   color: rgb(0, 0, 0),
+    // });
+
+    // Draw table content
+    const propertyLabels = ["Name", "Education", "ISBN Number", "ADDRESS"];
+    const properties = ["name", "education", "isbnNumber", "address"];
+
+    propertyLabels.forEach((label, rowIndex) => {
       const cellX = table.startX;
-      const cellY = tableStartY - rowIndex * table.rowHeight;
+      const cellY = table.startY - (rowIndex + 3) * table.rowHeight; // Add 2 to account for header and label row
 
-      page.drawLine({
-        start: { x: cellX, y: cellY },
-        end: { x: cellX, y: cellY - table.rowHeight },
-        thickness: table.borderWidth,
+      page.drawText(label, {
+        x: cellX + table.borderWidth + padding,
+        y: cellY - table.borderWidth - padding,
+        size: fontSize,
         color: rgb(0, 0, 0),
       });
 
-      page.drawLine({
-        start: { x: cellX + table.columnWidth, y: cellY },
-        end: { x: cellX + table.columnWidth, y: cellY - table.rowHeight },
-        thickness: table.borderWidth,
-        color: rgb(0, 0, 0),
-      });
-
-      if (rowIndex !== 0) {
-        page.drawLine({
-          start: { x: cellX, y: cellY - table.rowHeight },
-          end: {
-            x: cellX + table.columns * table.columnWidth,
-            y: cellY - table.rowHeight,
-          },
-          thickness: table.borderWidth,
-          color: rgb(0, 0, 0),
-        });
-      }
-
-      page.drawText(property, {
-        x: cellX + table.borderWidth,
-        y: cellY - table.borderWidth,
-        size: 12,
-        color: rgb(0, 0, 0),
-      });
-
-      const value = obj[property];
+      const value = object[properties[rowIndex]];
 
       page.drawText(value.toString(), {
-        x: cellX + table.columnWidth + table.borderWidth,
-        y: cellY - table.borderWidth,
-        size: 12,
+        x: cellX + table.columnWidth + table.borderWidth + padding,
+        y: cellY - table.borderWidth - padding,
+        size: fontSize,
         color: rgb(0, 0, 0),
       });
     });
+    count++;
   });
+
+  // Save the modified PDF
+  const modifiedPdfBytes = await pdfDoc.save();
+  return modifiedPdfBytes;
 };
 
 export default createPDF;
